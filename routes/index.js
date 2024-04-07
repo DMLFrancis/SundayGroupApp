665317cd050c2383a4bbe2fc174fad3f05dce528
DMLFrancis <dml.francis@gmail.com>
2024-04-07 18:52:12 +0100

    chnaged index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect("/math")
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
-
 
 module.exports = router;
