1c74675ede8671c1c6f3126992632cbf59ae73d9
DMLFrancis <dml.francis@gmail.com>
2024-04-07 19:10:17 +0100

    edited index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect("/math")
+  res.render('index', { title: "Group Application" });
 });
 
 
 module.exports = router;
+
